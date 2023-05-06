7d566da465630264becb2f32fe0b047be774ddfb
denizbas <deniz.bas@d-teknoloji.com.tr>
2023-05-06 14:30:50 +0300

    assets json files eklendi

---
+++ app/src/main/java/com/example/satellitesfinder/common/JsonFileName.kt
@@ -0,0 +1,9 @@
+package com.example.satellitesfinder.common
+
+annotation class JsonFileName {
+    companion object {
+        var POSITIONS = "Positions.json"
+        var SATELLITE_DETAIL = "Satellites_Detail.json"
+        var SATELLITE_LIST = "Satellites_List.json"
+    }
+}
